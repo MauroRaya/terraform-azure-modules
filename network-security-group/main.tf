1c0ef803006245263c08cb3214eeb7beeceeecd5
MauroRaya <maurorayabr@gmail.com>
2025-11-01 23:04:44 -0300

    feat: add network security group code

---
+++ network-security-group/main.tf
@@ -0,0 +1,6 @@
+resource "azurerm_network_security_group" "security_group" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  security_rule       = var.security_rule
+}
